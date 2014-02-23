5b1b256e7254e8ce6cae9a839483ed2cb4ecb9d3
beetlebit <giraybalci1108@gmail.com>
2014-02-23 03:25:09 +0200

    gitar project added.

---
+++ gitar/gitar.ino
@@ -0,0 +1,35 @@
+
+#include "pitches.h"
+int cnyOku = 2;
+int led = 4;
+int potOku = A0;
+int potDeger = 0;
+
+void setup(){  
+Serial.begin(9600);
+pinMode(cnyOku, INPUT);
+pinMode(led, OUTPUT);
+digitalWrite(led, LOW);
+
+}
+
+void loop(){
+int deger = digitalRead(cnyOku);
+potDeger = analogRead(potOku);
+potDeger = (potDeger*4.8)+30;
+if(deger == HIGH){
+  digitalWrite(led, HIGH);
+  tone(8,potDeger);  
+  //tone(pin, frequency) 
+//tone(pin, frequency, duration)
+}
+else{
+  digitalWrite(led, LOW);
+  noTone(8);
+}
+
+
+
+Serial.println(potDeger);
+delay(200);
+}
